8ebd1d044e2a6a339d98541097eae69916e934d5
Rui H Banki <ruihbanki@gmail.com>
2021-03-18 16:19:45 -0300

    Add utils to insert node

--- src/utils/treeUtils.spec.ts
+++ src/utils/treeUtils.spec.ts
@@ -1,4 +1,4 @@
-import { findNode } from "./treeUtils";
+import { findNode, findParentNode, insertNode } from "./treeUtils";
 
 const treeData = [
   { label: "Item 1", id: "1" },
@@ -23,6 +23,154 @@ describe("The treeUtils", function () {
     it("should return null node when it does not exist", function () {
       const result = findNode(treeData, "9999");
       expect(result).to.equal(null);
+    });
+  });
+
+  describe("findParentNode", function () {
+    it("should return the parent node when it exists", function () {
+      const result = findParentNode(treeData, "3");
+      expect(result.label).to.equal("Item 2");
+    });
+
+    it("should return null node when the parent does not exist", function () {
+      const result = findParentNode(treeData, "5");
+      expect(result).to.equal(null);
+    });
+
+    it("should return null node when pass a id that does not exist", function () {
+      const result = findParentNode(treeData, "99999");
+      expect(result).to.equal(null);
+    });
+  });
+
+  describe("insertNode", function () {
+    it("should insert the node before", function () {
+      const result = insertNode(treeData, "1", "before", {
+        label: "New item",
+        id: "6",
+      });
+
+      expect(result).to.deep.equal([
+        { label: "New item", id: "6" },
+        { label: "Item 1", id: "1" },
+        {
+          label: "Item 2",
+          id: "2",
+          children: [
+            { label: "Item 3", id: "3" },
+            { label: "Item 4", id: "4" },
+          ],
+        },
+        { label: "Item 5", id: "5" },
+      ]);
+    });
+
+    it("should insert the node before when it is inside another node", function () {
+      const result = insertNode(treeData, "4", "before", {
+        label: "New item",
+        id: "6",
+      });
+
+      expect(result).to.deep.equal([
+        { label: "Item 1", id: "1" },
+        {
+          label: "Item 2",
+          id: "2",
+          children: [
+            { label: "Item 3", id: "3" },
+            { label: "New item", id: "6" },
+            { label: "Item 4", id: "4" },
+          ],
+        },
+        { label: "Item 5", id: "5" },
+      ]);
+    });
+
+    it("should insert the node after", function () {
+      const result = insertNode(treeData, "1", "after", {
+        label: "New item",
+        id: "6",
+      });
+
+      expect(result).to.deep.equal([
+        { label: "Item 1", id: "1" },
+        { label: "New item", id: "6" },
+        {
+          label: "Item 2",
+          id: "2",
+          children: [
+            { label: "Item 3", id: "3" },
+            { label: "Item 4", id: "4" },
+          ],
+        },
+        { label: "Item 5", id: "5" },
+      ]);
+    });
+
+    it("should insert the node after when it is inside another node", function () {
+      const result = insertNode(treeData, "4", "after", {
+        label: "New item",
+        id: "6",
+      });
+
+      expect(result).to.deep.equal([
+        { label: "Item 1", id: "1" },
+        {
+          label: "Item 2",
+          id: "2",
+          children: [
+            { label: "Item 3", id: "3" },
+            { label: "Item 4", id: "4" },
+            { label: "New item", id: "6" },
+          ],
+        },
+        { label: "Item 5", id: "5" },
+      ]);
+    });
+
+    it("should insert the node inside", function () {
+      const result = insertNode(treeData, "1", "inside", {
+        label: "New item",
+        id: "6",
+      });
+
+      expect(result).to.deep.equal([
+        {
+          label: "Item 1",
+          id: "1",
+          children: [{ label: "New item", id: "6" }],
+        },
+        {
+          label: "Item 2",
+          id: "2",
+          children: [
+            { label: "Item 3", id: "3" },
+            { label: "Item 4", id: "4" },
+          ],
+        },
+        { label: "Item 5", id: "5" },
+      ]);
+    });
+
+    it("should insert the node inside in a nested structure", function () {
+      const result = insertNode(treeData, "2", "inside", {
+        label: "New item",
+        id: "6",
+      });
+
+      expect(result).to.deep.equal([
+        { label: "Item 1", id: "1" },
+        {
+          label: "Item 2",
+          id: "2",
+          children: [
+            { label: "New item", id: "6" },
+            { label: "Item 3", id: "3" },
+            { label: "Item 4", id: "4" },
+          ],
+        },
+        { label: "Item 5", id: "5" },
+      ]);
     });
   });
 });
--- src/utils/treeUtils.ts
+++ src/utils/treeUtils.ts
@@ -1,4 +1,5 @@
 import { Node } from "../components/DataTreeView/DataTreeView.types";
+import { Position } from "../components/TreeView/TreeView.types";
 
 type NodeData = {
   node: Node;
@@ -33,7 +34,113 @@ function createNodeDataByKeyInner(
   }
 }
 
+function findPath(nodeDataByKey: NodeDataByKey, id: string): Node[] {
+  let cur = nodeDataByKey[id];
+
+  if (!cur) {
+    return null;
+  }
+
+  const result = [];
+
+  /* eslint-disable */
+  while (true) {
+    result.unshift(cur.node);
+    if (!cur.parent) {
+      break;
+    }
+    cur = nodeDataByKey[cur.parent.id];
+  }
+
+  return result;
+}
+
+function insertNodeBeforeOrAfter(
+  treeData: Node[],
+  id: string,
+  position: Position,
+  node: Node
+): Node[] {
+  const nodeDataByKey = createNodeDataByKey(treeData);
+  const path = findPath(nodeDataByKey, id);
+  const result = [...treeData];
+
+  let childrenArr = result;
+  path.forEach((item) => {
+    const index = childrenArr.findIndex((i) => i.id === item.id);
+    if (item.id === id) {
+      if (position === "after") {
+        childrenArr.splice(index + 1, 0, node);
+      } else if (position === "before") {
+        childrenArr.splice(index, 0, node);
+      } else {
+        throw new Error("Invalid position");
+      }
+    } else {
+      childrenArr[index] = {
+        ...item,
+        children: [...childrenArr[index].children],
+      };
+      childrenArr = childrenArr[index].children;
+    }
+  });
+
+  return result;
+}
+
+function insertNodeInside(treeData: Node[], id: string, node: Node) {
+  const nodeDataByKey = createNodeDataByKey(treeData);
+  const path = findPath(nodeDataByKey, id);
+  const result = [...treeData];
+
+  let childrenArr = result;
+  path.forEach((item) => {
+    const index = childrenArr.findIndex((i) => i.id === item.id);
+    if (item.id === id) {
+      if (childrenArr[index].children) {
+        childrenArr[index] = {
+          ...item,
+          children: [node, ...childrenArr[index].children],
+        };
+      } else {
+        childrenArr[index] = {
+          ...item,
+          children: [node],
+        };
+      }
+    } else {
+      childrenArr[index] = {
+        ...item,
+        children: [...childrenArr[index].children],
+      };
+      childrenArr = childrenArr[index].children;
+    }
+  });
+
+  return result;
+}
+
+export function insertNode(
+  treeData: Node[],
+  id: string,
+  position: Position,
+  node: Node
+) {
+  if (position === "inside") {
+    return insertNodeInside(treeData, id, node);
+  }
+  return insertNodeBeforeOrAfter(treeData, id, position, node);
+}
+
 export function findNode(treeData: Node[], id: string): Node {
   const nodeDataByKey = createNodeDataByKey(treeData);
   return nodeDataByKey[id] ? nodeDataByKey[id].node : null;
+}
+
+export function findParentNode(treeData: Node[], id: string): Node | null {
+  const nodeDataByKey = createNodeDataByKey(treeData);
+  if (!nodeDataByKey[id]) {
+    return null;
+  }
+  return nodeDataByKey[id].parent;
 }
