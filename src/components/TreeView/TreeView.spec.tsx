34b2ab6b570d02b0eb2ac66bfbdcbb1c76be8b6b
Rui H Banki <ruihbanki@gmail.com>
2021-03-27 09:14:44 -0300

    Fix tree view tests

--- src/components/TreeView/TreeView.spec.tsx
+++ src/components/TreeView/TreeView.spec.tsx
@@ -15,17 +15,33 @@ describe("TreeView", function () {
     cy.contains("Item 3").should("exist");
   });
 
+  it("should render items without aria-grabbed when a tree view not is draggable", function () {
+    mountComponent();
+    cy.contains("Item 1")
+      .closest("li")
+      .invoke("attr", "aria-grabbed")
+      .should("equal", undefined);
+  });
+
+  it("should render items with aria-grabbed equal to false when a tree view is draggable", function () {
+    mountComponent({ draggable: true });
+    cy.contains("Item 1")
+      .closest("li")
+      .invoke("attr", "aria-grabbed")
+      .should("equal", "false");
+  });
+
   describe("on a mouse device", function () {
-    it("should set the attribute data-dragging to true when keep pressing the mouse", function () {
+    it("should set the attribute aria-grabbed to true when keep pressing the mouse", function () {
       mountComponent({ draggable: true });
       cy.contains("Item 2.1.1").trigger("mousedown");
       cy.contains("Item 2.1.1")
         .closest("li")
-        .invoke("attr", "data-dragging")
+        .invoke("attr", "aria-grabbed")
         .should("equal", "true");
     });
 
-    it("should not set the attribute data-dragging to true when pressing and release the mouse", function () {
+    it("should not set the attribute aria-grabbed to true when pressing and release the mouse", function () {
       mountComponent({ draggable: true });
       cy.contains("Item 2.1.1")
         .trigger("mousedown")
@@ -34,11 +50,11 @@ describe("TreeView", function () {
       cy.wait(500)
         .contains("Item 2.1.1")
         .closest("li")
-        .invoke("attr", "data-dragging")
+        .invoke("attr", "aria-grabbed")
         .should("equal", "false");
     });
 
-    it("should not set the attribute data-dragging to true when pressing and move the mouse", function () {
+    it("should not set the attribute aria-grabbed to true when pressing and move the mouse", function () {
       mountComponent({ draggable: true });
       cy.contains("Item 2.1.1")
         .trigger("mousedown")
@@ -47,33 +63,70 @@ describe("TreeView", function () {
       cy.wait(500)
         .contains("Item 2.1.1")
         .closest("li")
-        .invoke("attr", "data-dragging")
+        .invoke("attr", "aria-grabbed")
         .should("equal", "false");
     });
 
-    it("should not select when start dragging an item", function () {
-      mountComponent({ draggable: true });
-      cy.contains("Item 2.1.1").trigger("mousedown");
-      cy.wait(500)
-        .contains("Item 2.1.1")
-        .closest("li")
-        .trigger("click")
-        .invoke("attr", "aria-selected")
-        .should("not.exist");
+    it("should call onNodeDrop when drop before a node", function () {
+      const onNodeDrop = cy.stub();
+      mountComponent({ draggable: true, onNodeDrop });
+      cy.contains("Item 3").trigger("mousedown").wait(500);
+      cy.contains("Item 2.1.1").trigger("mousemove", "topLeft");
+      cy.contains("Item 3")
+        .trigger("mouseup")
+        .then(() => {
+          expect(onNodeDrop).to.be.calledWith({
+            fromNodeId: "item-3",
+            toNodeId: "item-2-1-1",
+            position: "before",
+          });
+        });
+    });
+
+    it("should call onNodeDrop when drop after a node", function () {
+      const onNodeDrop = cy.stub();
+      mountComponent({ draggable: true, onNodeDrop });
+      cy.contains("Item 3").trigger("mousedown").wait(500);
+      cy.contains("Item 2.1.1").trigger("mousemove", "bottomLeft");
+      cy.contains("Item 3")
+        .trigger("mouseup")
+        .then(() => {
+          expect(onNodeDrop).to.be.calledWith({
+            fromNodeId: "item-3",
+            toNodeId: "item-2-1-1",
+            position: "after",
+          });
+        });
+    });
+
+    it("should call onNodeDrop when drop inside a node", function () {
+      const onNodeDrop = cy.stub();
+      mountComponent({ draggable: true, onNodeDrop });
+      cy.contains("Item 3").trigger("mousedown").wait(500);
+      cy.contains("Item 2.1.1").trigger("mousemove", "bottomRight");
+      cy.contains("Item 3")
+        .trigger("mouseup")
+        .then(() => {
+          expect(onNodeDrop).to.be.calledWith({
+            fromNodeId: "item-3",
+            toNodeId: "item-2-1-1",
+            position: "inside",
+          });
+        });
     });
   });
 
   describe("on a touch device", function () {
-    it("should set the attribute data-dragging to true when keep touching", function () {
+    it("should set the attribute aria-grabbed to true when keep touching", function () {
       mountComponent({ draggable: true });
       cy.contains("Item 2.1.1").trigger("touchstart");
       cy.contains("Item 2.1.1")
         .closest("li")
-        .invoke("attr", "data-dragging")
+        .invoke("attr", "aria-grabbed")
         .should("equal", "true");
     });
 
-    it("should not set the attribute data-dragging to true when touch and release", function () {
+    it("should not set the attribute aria-grabbed to true when touch and release", function () {
       mountComponent({ draggable: true });
       cy.contains("Item 2.1.1")
         .trigger("touchstart")
@@ -82,9 +135,57 @@ describe("TreeView", function () {
       cy.wait(500)
         .contains("Item 2.1.1")
         .closest("li")
-        .invoke("attr", "data-dragging")
+        .invoke("attr", "aria-grabbed")
         .should("equal", "false");
     });
+
+    it.skip("should call onNodeDrop when drop before a node", function () {
+      const onNodeDrop = cy.stub();
+      mountComponent({ draggable: true, onNodeDrop });
+      cy.contains("Item 3").trigger("touchstart").wait(500);
+      cy.contains("Item 2.1.1").trigger("touchmove", "topLeft");
+      cy.contains("Item 3")
+        .trigger("touchend")
+        .then(() => {
+          expect(onNodeDrop).to.be.calledWith({
+            fromNodeId: "item-3",
+            toNodeId: "item-2-1-1",
+            position: "before",
+          });
+        });
+    });
+
+    // it("should call onNodeDrop when drop after a node", function () {
+    //   const onNodeDrop = cy.stub();
+    //   mountComponent({ draggable: true, onNodeDrop });
+    //   cy.contains("Item 3").trigger("mousedown").wait(500);
+    //   cy.contains("Item 2.1.1").trigger("mousemove", "bottomLeft");
+    //   cy.contains("Item 3")
+    //     .trigger("mouseup")
+    //     .then(() => {
+    //       expect(onNodeDrop).to.be.calledWith({
+    //         fromNodeId: "item-3",
+    //         toNodeId: "item-2-1-1",
+    //         position: "after",
+    //       });
+    //     });
+    // });
+
+    // it("should call onNodeDrop when drop inside a node", function () {
+    //   const onNodeDrop = cy.stub();
+    //   mountComponent({ draggable: true, onNodeDrop });
+    //   cy.contains("Item 3").trigger("mousedown").wait(500);
+    //   cy.contains("Item 2.1.1").trigger("mousemove", "bottomRight");
+    //   cy.contains("Item 3")
+    //     .trigger("mouseup")
+    //     .then(() => {
+    //       expect(onNodeDrop).to.be.calledWith({
+    //         fromNodeId: "item-3",
+    //         toNodeId: "item-2-1-1",
+    //         position: "inside",
+    //       });
+    //     });
+    // });
   });
 });
 
