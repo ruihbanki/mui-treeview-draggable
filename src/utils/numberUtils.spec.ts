e0345c896b46fc17f862cba7b332e32c129bc87b
Rui H Banki <ruihbanki@gmail.com>
2021-03-27 08:31:10 -0300

    Remove unused files

--- src/utils/numberUtils.spec.ts
+++
@@ -1,7 +0,0 @@
-import { addNumbers } from "./numberUtils";
-
-describe("addNumbers", function () {
-  it("should return 5 when add 2 and 3", function () {
-    expect(addNumbers(2, 3)).to.equal(5);
-  });
-});
--- src/utils/numberUtils.ts
+++
@@ -1,3 +0,0 @@
-export function addNumbers(n1: number, n2: number): number {
-  return n1 + n2;
-}
